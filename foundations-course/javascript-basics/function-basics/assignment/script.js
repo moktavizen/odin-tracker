a8c419c6f426facea45879d7262f384f73f8fa98
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-06-12 16:39:25 +0700

    feat: add assignment for Function Basics

---
+++ foundations-course/javascript-basics/function-basics/assignment/script.js
@@ -0,0 +1,26 @@
+// For now, just write each function and test the output with console.log.
+//
+//   Write a function called add7 that takes one number and returns that number + 7.
+//   Write a function called multiply that takes 2 numbers and returns their product.
+//   Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+//   Write a function called lastLetter that takes a string and returns the very last letter of that string:
+//     lastLetter("abcd") should return "d"
+
+function add7(num1) {
+  return num1 + 7;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function capitalize(str) {
+  const firstChar = str.at(0).toUpperCase();
+  const restOfString = str.slice(1).toLowerCase();
+
+  return firstChar + restOfString;
+}
+
+function lastLetter(text) {
+  return text.slice(-1);
+}
