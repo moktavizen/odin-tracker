eabb1fc53119d64509be8f50e32abd3affc24307
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-07-21 17:16:37 +0700

    feat: update loops and array exercise

---
+++ foundations-course/javascript-basics/loops-and-arrays/exercises/06_repeatString/repeatString.js
@@ -0,0 +1,16 @@
+const repeatString = (str, n) => {
+  if (n < 0) {
+    return "ERROR";
+  }
+
+  let repeatedString = "";
+
+  for (let i = 0; i < n; i++) {
+    repeatedString += str;
+  }
+
+  return repeatedString;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
---
+++ foundations-course/javascript-basics/loops-and-arrays/exercises/07_reverseString/reverseString.js
@@ -0,0 +1,4 @@
+const reverseString = (str) => str.split("").reverse().join("");
+
+// Do not edit below this line
+module.exports = reverseString;
