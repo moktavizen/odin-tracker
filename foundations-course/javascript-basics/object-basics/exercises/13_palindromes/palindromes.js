4bbaa6ca3756ec81ab151c076d29add8074b8990
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-12-19 13:27:37 +0700

    Finish `13_palindromes`

--- foundations-course/javascript-basics/object-basics/exercises/13_palindromes/palindromes.js
+++ foundations-course/javascript-basics/object-basics/exercises/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = (string) => {
+  // RegEx approach
+  const cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  // Filter approach
+  // const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+  // const cleanedString = string
+  //   .toLowerCase()
+  //   .split("")
+  //   .filter((char) => alphanumeric.includes(char))
+  //   .join("");
+
+  const reversedString = cleanedString.split("").reverse().join("");
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
