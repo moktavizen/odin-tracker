d8123af6bdf3dccc3e59ebedd7c3ceecc038764e
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-12-19 14:53:47 +0700

    Finish `16_findTheOldest`

--- foundations-course/javascript-basics/object-basics/exercises/16_findTheOldest/findTheOldest.js
+++ foundations-course/javascript-basics/object-basics/exercises/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+  const validPeople = people.map((person) => ({
+    name: person.name,
+    yearOfBirth: person.yearOfBirth,
+    yearOfDeath: person.yearOfDeath || new Date().getFullYear(),
+  }));
 
+  const sortByOldest = validPeople.sort((personA, personB) => {
+    const a = personA.yearOfDeath - personA.yearOfBirth;
+    const b = personB.yearOfDeath - personB.yearOfBirth;
+
+    return b - a;
+  });
+
+  return sortByOldest[0];
 };
 
 // Do not edit below this line
