553f3558d62394796846976ea7d260e1e2903fbe
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-12-19 14:07:16 +0700

    Finish `14_fibonacci`

--- foundations-course/javascript-basics/object-basics/exercises/14_fibonacci/fibonacci.js
+++ foundations-course/javascript-basics/object-basics/exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = (n) => {
+  if (typeof n === "string") {
+    n = Number(n);
+  }
 
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  if (n === 2) {
+    return 1;
+  }
+
+  let f;
+  let n1 = 1;
+  let n2 = 1;
+  for (let i = 3; i <= n; i++) {
+    f = n1 + n2;
+    n1 = n2;
+    n2 = f;
+  }
+
+  return f;
 };
 
 // Do not edit below this line
