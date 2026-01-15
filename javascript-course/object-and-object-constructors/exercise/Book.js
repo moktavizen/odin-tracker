66e6b95c349e820173808ae003c58d46b52d8a0c
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-01-15 19:47:30 +0700

    Complete "Objects and Object Constructors" exercise

---
+++ javascript-course/object-and-object-constructors/exercise/Book.js
@@ -0,0 +1,17 @@
+// theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+// console.log(theHobbit.info());
+
+function Book(title, author, pageCount, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
+  this.info = function () {
+    const info = `${this.title} by ${this.author}, ${this.pageCount} pages, ${this.readStatus}`;
+    return info;
+  };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
+
+console.log(theHobbit.info());
